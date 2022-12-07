6808cba67df534c26088e82841f10d4ca7cf7ff2
Your Name <beksaev85@mail.ru>
2022-12-07 16:15:57 +0400

    1 commit

---
+++ app/Http/Controllers/Admin/Main/BlogSingle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogSingle extends Controller
+{
+    public function __invoke()
+    {
+        return view('111');
+    }
+}
